5396bc1ec897b493dd0c520feb446c29535c0e4d
Kaiser Kamruzzaman <38045495+KaiserKamruzzaman@users.noreply.github.com>
2021-12-11 12:37:53 +0100

    authentication added partially
    firebase authentication added

--- react-auth/components/login/LoginForm.js
+++ react-auth/components/login/LoginForm.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import classes from "./LoginForm.module.css";
 
-
 const LoginForm = () => {
-
   const [isLogin, setIsLogin] = useState(true);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -12,16 +12,61 @@ const LoginForm = () => {
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
-   
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    // console.log(enteredEmail);
+    // console.log(enteredPassword);
+    let url;
+
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBT04MUIPhtUNDySJT6t9sUntwJHI2LJzE";
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBT04MUIPhtUNDySJT6t9sUntwJHI2LJzE";
+    }
+
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            return res.json().then((data) => {
+              let errorMessage = "Authentication failed...";
+              if (data && data.error && data.error.message) {
+                errorMessage = data.error.message;
+              }
+              throw new Error(errorMessage);
+            });
+          }
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => alert(error.message));
+
+
+
   };
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form>
+      <form onSubmit={formSubmitHandler}>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required  />
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="password">Your Password</label>
@@ -29,6 +74,7 @@ const LoginForm = () => {
             type="password"
             id="password"
             required
+            ref={passwordInputRef}
           />
         </div>
         <div className={classes.actions}>
